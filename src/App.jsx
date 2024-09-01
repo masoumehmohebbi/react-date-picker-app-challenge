5c86949cda30e4a214300aad9b6ec5de5bdc4a25
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-09-01 12:50:50 +0330

    updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -140,7 +140,7 @@ export default App;
 
 function CalculateDate({ label, children, icon }) {
   return (
-    <div className="flex gap-x-3 mt-11 hover:scale-105 pb-3 hover:bg-pink-400 hover:ring-pink-400 transition duration-500 bg-pink-300 px-5 py-4 rounded-md shadow-xl ring-offset-1 ring ring-pink-300">
+    <div className="flex border border-pink-400 gap-x-3 mt-11 hover:scale-105 pb-3 hover:bg-pink-400 hover:ring-pink-400 transition duration-500 bg-pink-300 px-5 py-4 rounded-md shadow-xl ring-offset-2 ring ring-pink-400">
       <motion.p
         whileHover={{
           scale: 1.1,
--- ui/DatePickerField.jsx
+++ ui/DatePickerField.jsx
@@ -11,14 +11,14 @@ const DatePickerField = ({ label, date, setDate }) => {
         initial={{ x: "-70px" }}
         animate={{ x: "0" }}
         transition={{ duration: 1, type: "spring", stiffness: 120 }}
-        className="mb-4  text-slate-700 font-black text-xl pb-5 flex items-center gap-x-2"
+        className="mb-4 text-slate-700 font-black text-xl pb-5 flex items-center gap-x-2"
       >
         <MdDateRange className="w-7 h-7 text-pink-700" />
         {label}
       </motion.h1>
 
       <DatePicker
-        containerClassName="w-full group bg-pink-200 hover:bg-pink-300 border border-purple-400 ring-1 ring-purple-400 ring-offset-2 rounded-md shadow-xl py-1 cursor-pointer pr-20"
+        containerClassName="w-full group bg-pink-200 hover:bg-pink-300 border border-purple-400 ring-2 ring-purple-500 ring-offset-2 rounded-md shadow-xl py-1 cursor-pointer pr-24"
         inputClass="textField__input bg-pink-200 group-hover:bg-pink-300 text-lg"
         calenderPosition="bottom-center"
         value={date}
