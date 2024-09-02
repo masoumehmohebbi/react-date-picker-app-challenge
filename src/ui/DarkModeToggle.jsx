9eb7e36fc8c5b33773d7080391af974a78495c50
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-09-02 22:13:48 +0330

    Added darkMode color

--- src/ui/DarkModeToggle.jsx
+++ src/ui/DarkModeToggle.jsx
@@ -5,9 +5,9 @@ const DarkModeToggle = () => {
   return (
     <button onClick={toggleDarkMode}>
       {isDarkMode ? (
-        <HiOutlineSun className="w-7 h-7 text-primary-900" />
+        <HiOutlineSun className="w-8 h-8 text-primary-900" />
       ) : (
-        <HiOutlineMoon className="w-7 h-7 text-primary-900" />
+        <HiOutlineMoon className="w-8 h-8 text-primary-900" />
       )}
     </button>
   );
--- src/ui/Haeder.jsx
+++ src/ui/Haeder.jsx
@@ -4,7 +4,7 @@ const Haeder = () => {
   return (
     <nav
       className={`py-3 w-full bg-secondary-0 transiton-all duration-100 ease-out z-20 shadow-menu md:shadow-none backdrop-blur-2xl
-    blur-0 opacity-100 sticky top-0 font-sans items-center px-2 sm:px-3 flex justify-between`}
+    blur-0 opacity-100 sticky top-0 font-sans items-center px-2 sm:px-5 flex justify-between`}
     >
       <div className="flex justify-between w-full">
         <h1 className="font-black text-[27px] text-primary-900"> با حسابــ</h1>
